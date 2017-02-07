eb70c4045bad0fe19c17b908e2600b340ac3f331
rsJuuuuu <juuuuu@hotmail.fi>
2017-02-07 17:30:01 +0200

    Fix crash if issue has no assignee

--- src/net/orekyuu/bitbucketissues/IssueTableModel.java
+++ src/net/orekyuu/bitbucketissues/IssueTableModel.java
@@ -6,7 +6,8 @@ import javax.swing.table.DefaultTableModel;
 
 public class IssueTableModel extends DefaultTableModel {
 
-    private static String[] TITLE = {"Issue Number", "Title", "Type", "Priority", "Status", "Responsible", "Created", "Updated"};
+    private static String[] TITLE = {"Issue Number", "Title", "Type", "Priority", "Status", "Responsible", "Created",
+            "Updated"};
 
     public IssueTableModel() {
         super(TITLE, 0);
@@ -19,12 +20,16 @@ public class IssueTableModel extends DefaultTableModel {
     }
 
     public void addRow(Issue issue) {
-        addRow(new Object[]{issue.getLocal_id(), issue.getTitle(), issue.getMetadata().getKind(), issue.getPriority(), issue.getStatus(), issue.getResponsible().getDisplay_name(), issue.getCreated_on(), issue.getUtc_last_updated()});
+        addRow(new Object[]{issue.getLocal_id(), issue.getTitle(), issue.getMetadata().getKind(), issue.getPriority()
+                , issue.getStatus(),
+                issue.getResponsible()
+                != null ? issue.getResponsible().getDisplay_name() : "Not set", issue.getCreated_on(), issue
+                .getUtc_last_updated()});
     }
 
     public void clear() {
         int rows = getRowCount();
-        for(int i = rows - 1; i >=0; i--) {
+        for (int i = rows - 1; i >= 0; i--) {
             removeRow(i);
         }
     }
